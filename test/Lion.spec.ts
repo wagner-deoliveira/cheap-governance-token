296e00eb8d6c0052b7904261b85544f435466313
Wagner Rosa <wagner.deoliveira@perkinelmer.com>
2021-05-28 22:17:17 +0200

    Add more refactoring to the fork

--- test/Lion.spec.ts
+++ test/Lion.spec.ts
@@ -6,7 +6,7 @@ import { ecsign } from 'ethereumjs-util'
 import { governanceFixture } from './fixtures'
 import { expandTo18Decimals, mineBlock } from './utils'
 
-import Uni from '../build/Uni.json'
+import Lion from '../build/Lion.json'
 
 chai.use(solidity)
 
@@ -18,7 +18,7 @@ const PERMIT_TYPEHASH = utils.keccak256(
   utils.toUtf8Bytes('Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)')
 )
 
-describe('Uni', () => {
+describe('Lion', () => {
   const provider = new MockProvider({
     ganacheOptions: {
       hardfork: 'istanbul',
@@ -29,24 +29,24 @@ describe('Uni', () => {
   const [wallet, other0, other1] = provider.getWallets()
   const loadFixture = createFixtureLoader([wallet], provider)
 
-  let uni: Contract
+  let lion: Contract
   beforeEach(async () => {
     const fixture = await loadFixture(governanceFixture)
-    uni = fixture.uni
+    lion = fixture.lion
   })
 
   it('permit', async () => {
     const domainSeparator = utils.keccak256(
       utils.defaultAbiCoder.encode(
         ['bytes32', 'bytes32', 'uint256', 'address'],
-        [DOMAIN_TYPEHASH, utils.keccak256(utils.toUtf8Bytes('Uniswap')), 1, uni.address]
+        [DOMAIN_TYPEHASH, utils.keccak256(utils.toUtf8Bytes('Cheapswap')), 1, lion.address]
       )
     )
 
     const owner = wallet.address
     const spender = other0.address
     const value = 123
-    const nonce = await uni.nonces(wallet.address)
+    const nonce = await lion.nonces(wallet.address)
     const deadline = constants.MaxUint256
     const digest = utils.keccak256(
       utils.solidityPack(
@@ -67,57 +67,57 @@ describe('Uni', () => {
 
     const { v, r, s } = ecsign(Buffer.from(digest.slice(2), 'hex'), Buffer.from(wallet.privateKey.slice(2), 'hex'))
 
-    await uni.permit(owner, spender, value, deadline, v, utils.hexlify(r), utils.hexlify(s))
-    expect(await uni.allowance(owner, spender)).to.eq(value)
-    expect(await uni.nonces(owner)).to.eq(1)
+    await lion.permit(owner, spender, value, deadline, v, utils.hexlify(r), utils.hexlify(s))
+    expect(await lion.allowance(owner, spender)).to.eq(value)
+    expect(await lion.nonces(owner)).to.eq(1)
 
-    await uni.connect(other0).transferFrom(owner, spender, value)
+    await lion.connect(other0).transferFrom(owner, spender, value)
   })
 
   it('nested delegation', async () => {
-    await uni.transfer(other0.address, expandTo18Decimals(1))
-    await uni.transfer(other1.address, expandTo18Decimals(2))
+    await lion.transfer(other0.address, expandTo18Decimals(1))
+    await lion.transfer(other1.address, expandTo18Decimals(2))
 
-    let currectVotes0 = await uni.getCurrentVotes(other0.address)
-    let currectVotes1 = await uni.getCurrentVotes(other1.address)
+    let currectVotes0 = await lion.getCurrentVotes(other0.address)
+    let currectVotes1 = await lion.getCurrentVotes(other1.address)
     expect(currectVotes0).to.be.eq(0)
     expect(currectVotes1).to.be.eq(0)
 
-    await uni.connect(other0).delegate(other1.address)
-    currectVotes1 = await uni.getCurrentVotes(other1.address)
+    await lion.connect(other0).delegate(other1.address)
+    currectVotes1 = await lion.getCurrentVotes(other1.address)
     expect(currectVotes1).to.be.eq(expandTo18Decimals(1))
 
-    await uni.connect(other1).delegate(other1.address)
-    currectVotes1 = await uni.getCurrentVotes(other1.address)
+    await lion.connect(other1).delegate(other1.address)
+    currectVotes1 = await lion.getCurrentVotes(other1.address)
     expect(currectVotes1).to.be.eq(expandTo18Decimals(1).add(expandTo18Decimals(2)))
 
-    await uni.connect(other1).delegate(wallet.address)
-    currectVotes1 = await uni.getCurrentVotes(other1.address)
+    await lion.connect(other1).delegate(wallet.address)
+    currectVotes1 = await lion.getCurrentVotes(other1.address)
     expect(currectVotes1).to.be.eq(expandTo18Decimals(1))
   })
 
   it('mints', async () => {
     const { timestamp: now } = await provider.getBlock('latest')
-    const uni = await deployContract(wallet, Uni, [wallet.address, wallet.address, now + 60 * 60])
-    const supply = await uni.totalSupply()
+    const lion = await deployContract(wallet, Lion, [wallet.address, wallet.address, now + 60 * 60])
+    const supply = await lion.totalSupply()
 
-    await expect(uni.mint(wallet.address, 1)).to.be.revertedWith('Uni::mint: minting not allowed yet')
+    await expect(lion.mint(wallet.address, 1)).to.be.revertedWith('Lion::mint: minting not allowed yet')
 
-    let timestamp = await uni.mintingAllowedAfter()
+    let timestamp = await lion.mintingAllowedAfter()
     await mineBlock(provider, timestamp.toString())
 
-    await expect(uni.connect(other1).mint(other1.address, 1)).to.be.revertedWith('Uni::mint: only the minter can mint')
-    await expect(uni.mint('0x0000000000000000000000000000000000000000', 1)).to.be.revertedWith('Uni::mint: cannot transfer to the zero address')
+    await expect(lion.connect(other1).mint(other1.address, 1)).to.be.revertedWith('Lion::mint: only the minter can mint')
+    await expect(lion.mint('0x0000000000000000000000000000000000000000', 1)).to.be.revertedWith('Lion::mint: cannot transfer to the zero address')
 
     // can mint up to 2%
-    const mintCap = BigNumber.from(await uni.mintCap())
+    const mintCap = BigNumber.from(await lion.mintCap())
     const amount = supply.mul(mintCap).div(100)
-    await uni.mint(wallet.address, amount)
-    expect(await uni.balanceOf(wallet.address)).to.be.eq(supply.add(amount))
+    await lion.mint(wallet.address, amount)
+    expect(await lion.balanceOf(wallet.address)).to.be.eq(supply.add(amount))
 
-    timestamp = await uni.mintingAllowedAfter()
+    timestamp = await lion.mintingAllowedAfter()
     await mineBlock(provider, timestamp.toString())
     // cannot mint 2.01%
-    await expect(uni.mint(wallet.address, supply.mul(mintCap.add(1)))).to.be.revertedWith('Uni::mint: exceeded mint cap')
+    await expect(lion.mint(wallet.address, supply.mul(mintCap.add(1)))).to.be.revertedWith('Lion::mint: exceeded mint cap')
   })
 })
