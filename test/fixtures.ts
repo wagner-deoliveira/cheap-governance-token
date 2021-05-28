2393e7748eff14baac307913ed77943bd4409935
Wagner Rosa <wagner.deoliveira@perkinelmer.com>
2021-05-28 22:20:46 +0200

    Change Uni for Lion in fixtures.ts

--- test/fixtures.ts
+++ test/fixtures.ts
@@ -2,7 +2,7 @@ import chai, { expect } from 'chai'
 import { Contract, Wallet, providers } from 'ethers'
 import { solidity, deployContract } from 'ethereum-waffle'
 
-import Uni from '../build/Uni.json'
+import Lion from '../build/Lion.json'
 import Timelock from '../build/Timelock.json'
 import GovernorAlpha from '../build/GovernorAlpha.json'
 
@@ -11,7 +11,7 @@ import { DELAY } from './utils'
 chai.use(solidity)
 
 interface GovernanceFixture {
-  uni: Contract
+  lion: Contract
   timelock: Contract
   governorAlpha: Contract
 }
@@ -20,10 +20,10 @@ export async function governanceFixture(
   [wallet]: Wallet[],
   provider: providers.Web3Provider
 ): Promise<GovernanceFixture> {
-  // deploy UNI, sending the total supply to the deployer
+  // deploy Lion, sending the total supply to the deployer
   const { timestamp: now } = await provider.getBlock('latest')
   const timelockAddress = Contract.getContractAddress({ from: wallet.address, nonce: 1 })
-  const uni = await deployContract(wallet, Uni, [wallet.address, timelockAddress, now + 60 * 60])
+  const lion = await deployContract(wallet, Lion, [wallet.address, timelockAddress, now + 60 * 60])
 
   // deploy timelock, controlled by what will be the governor
   const governorAlphaAddress = Contract.getContractAddress({ from: wallet.address, nonce: 2 })
@@ -31,8 +31,8 @@ export async function governanceFixture(
   expect(timelock.address).to.be.eq(timelockAddress)
 
   // deploy governorAlpha
-  const governorAlpha = await deployContract(wallet, GovernorAlpha, [timelock.address, uni.address])
+  const governorAlpha = await deployContract(wallet, GovernorAlpha, [timelock.address, lion.address])
   expect(governorAlpha.address).to.be.eq(governorAlphaAddress)
 
-  return { uni, timelock, governorAlpha }
+  return { lion, timelock, governorAlpha }
 }
