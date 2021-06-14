d0a2be8c9d42da89332afd7c966deaa82073eeeb
Wagner Rosa <wagner.deoliveira@perkinelmer.com>
2021-06-14 17:16:55 +0200

    Updates

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -11,11 +11,14 @@ async function main() {
   console.log("Account balance:", (await deployer.getBalance()).toString());
   
   const Pony = await ethers.getContractFactory("Pony");
-  const pony = await Pony.deploy(process.env.DEPLOY_WALLET, process.env.MINT_ACCOUNT, process.env.MINTING_ALLOWED_AFTER);
+  const pony = await Pony.deploy(process.env.DEPLOY_WALLET, process.env.MINT_ACCOUNT, Date.now());
 
   await pony.deployed();
+  await Pony['mint(address,uint)'](123);
+  //await pony.mint(process.env.DEPLOY_WALLET, 1000000000);
 
   console.log("Pony deployed to:", pony.address);
+  //console.log("Pony minted to:", pony.address);
 }
 
 main()
--- scripts/mint.ts
+++ scripts/mint.ts
@@ -2,13 +2,39 @@ const { ethers } = require("hardhat");
 require('dotenv').config()
 
 async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log(
+    "Deploying contracts with the account:",
+    deployer.address
+  );
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+  
+  // deploy Pony, sending the total supply to the deployer
+  const now = Date.now();
+  const wallet = process.env.DEPLOY_WALLET;
+  const minter = process.env.MINT_ACCOUNT;
+
 
   const Pony = await ethers.getContractFactory("Pony");
-  const pony = await Pony.deploy(process.env.DEPLOY_WALLET, process.env.MINT_ACCOUNT, process.env.MINTING_ALLOWED_AFTER);
+  const pony = await Pony.deploy(wallet, minter, now);
 
-  pony.mint(process.env.MINT_ACCOUNT, 1000000000);
+  // deploy timelock, controlled by what will be the governor
+  const Timelock = await ethers.getContractFactory("Timelock");
+  const timelock = await Timelock.deploy(wallet, now)
 
-  console.log("Pony minted to:", pony.address);
+  // deploy governorAlpha
+  const GovernorAlpha = await ethers.getContractFactory("GovernorAlpha");
+  const governor = await GovernorAlpha.deploy(wallet, GovernorAlpha, [timelock.address, pony.address])
+
+  await pony.deployed();
+  await timelock.deployed();
+  await governor.deployed();
+  //await Pony['mint(address,uint)'](123);
+  //await pony.mint(process.env.DEPLOY_WALLET, 1000000000);
+
+  console.log("Pony deployed to:", pony.address);
+  //console.log("Pony minted to:", pony.address);
 }
 
 main()
